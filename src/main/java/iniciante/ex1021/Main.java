28c831560db3e95dc18c5dc331cfce44c40de07e
patrick-martins <xavier.patrickmartins@gmail.com>
2023-08-10 17:47:55 -0300

    página de execução do programa

---
+++ src/main/java/iniciante/ex1021/Main.java
@@ -0,0 +1,21 @@
+package iniciante.ex1021;
+
+import java.io.IOException;
+import java.security.InvalidParameterException;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        Scanner input = new Scanner(System.in);
+        TratamentoDoValor tratar = new TratamentoDoValor();
+
+        double valorMonetario = input.nextDouble();
+
+        if (!tratar.verificaDisponiblidadeNumero(valorMonetario))
+            throw new InvalidParameterException("Número inválido!");
+        else
+            tratar.printaQuantidadeMinimaDeNotas(valorMonetario);
+
+    }
+}
