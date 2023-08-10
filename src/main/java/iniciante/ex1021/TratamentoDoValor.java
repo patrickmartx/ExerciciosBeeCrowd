bcc74f10a39ecf2abd527234d445d08640739e57
patrick-martins <xavier.patrickmartins@gmail.com>
2023-08-10 17:48:50 -0300

    classe responsável por fazer o tratamendo do valor, separando notas e moedas.

---
+++ src/main/java/iniciante/ex1021/TratamentoDoValor.java
@@ -0,0 +1,65 @@
+package iniciante.ex1021;
+
+public class TratamentoDoValor {
+
+    public boolean verificaDisponiblidadeNumero(double numero) {
+        if ((numero < 0) || (numero > 1000000)) {
+            return false;
+        }
+        return true;
+    }
+
+    public static void printaQuantidadeMinimaDeNotas(double valor) {
+        double[] notas = { 100.00, 50.00, 20.00, 10.00, 5.00, 2.00 };
+        double[] moedas = { 1.00, 0.50, 0.25, 0.10, 0.05, 0.01 };
+
+        int[] quantidadeNotas = separaQuantidadeDeNotas(notas, valor);
+        int[] quantidadeMoedas = separaQuantidadeDeMoedas(moedas, valor);
+
+        System.out.println("NOTAS:");
+
+        for (int i = 0; i < quantidadeNotas.length; i++) {
+            System.out.printf("%d nota(s) de R$ %.2f\n", quantidadeNotas[i], notas[i]);
+        }
+
+        System.out.println("MOEDAS:");
+
+        for (int i = 0; i < quantidadeNotas.length; i++) {
+            System.out.printf("%d moeda(s) de R$ %.2f\n", quantidadeMoedas[i], moedas[i]);
+        }
+    }
+
+    public static int[] separaQuantidadeDeNotas(double[] notas, double valor) {
+        int notasDisponiveis = 6;
+
+        int[] quantidadeNotas = new int[notasDisponiveis];
+
+        for (int i = 0; i < quantidadeNotas.length; i++) {
+            quantidadeNotas[i] = (int) (valor / notas[i]);
+            valor %= notas[i];
+        }
+        return quantidadeNotas;
+    }
+
+    public static int[] separaQuantidadeDeMoedas(double[] moedas, double valor) {
+        int casaDasUnidades = 10;
+        int cincoReaisUsados = 5;
+        int doisReaisUsados = 2;
+
+        int notasDisponiveis = 6;
+        int converteCentavosEmReais = 100;
+
+        valor = (((valor % casaDasUnidades) % cincoReaisUsados) % doisReaisUsados);
+
+        double valorComCentavos = valor * converteCentavosEmReais;
+
+        int[] quantiadeMoedas = new int[notasDisponiveis];
+
+        for (int i = 0; i < quantiadeMoedas.length; i++) {
+            quantiadeMoedas[i] = (int) (valorComCentavos / (moedas[i] * converteCentavosEmReais));
+            valorComCentavos %= (moedas[i] * converteCentavosEmReais);
+        }
+
+        return quantiadeMoedas;
+    }
+}
